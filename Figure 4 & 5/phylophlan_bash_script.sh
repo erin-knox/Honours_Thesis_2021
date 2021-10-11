27aeaba02a02a112db0b00c65750316317b9d331
Erin Knox <81397498+erin-knox@users.noreply.github.com>
2021-10-11 19:26:36 +1300

    Add files via upload

---
+++ Figure 4 & 5/phylophlan_bash_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#PhyloPhlan
+
+#WD=/Users/erinknox/Desktop/PhyloPhlan
+
+#loc=Test
+
+
+phylophlan -i /Users/erinknox/Desktop/PhyloPhlan/Cut_ORFs -d phylophlan --diversity medium -f custom_test.cfg --proteome_extension .fasta --verbose
